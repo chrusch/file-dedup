a5399cdd7b0997906ca5c35e5729fa92e2a072ad
Clayton Chrusch <chrusch@gmail.com>
2023-01-01 17:54:01 -0500

    fill out stub test for dedup.ts

--- tests/dedup.test.ts
+++ tests/dedup.test.ts
@@ -6,23 +6,66 @@
 
 import {dedup, DedupOptions} from '../src/dedup';
 import {silenceOutput} from '../src/display';
+import * as gcf from '../src/get_candidate_files';
+import * as hash_file from '../src/hash_files';
+import * as duplicates from '../src/duplicates';
+import * as rd from '../src/remove_duplicates';
+import {jest} from '@jest/globals'; // needed for jest.Mocked
+
+jest.mock('../src/get_candidate_files.ts');
+jest.mock('../src/hash_files.ts');
+jest.mock('../src/remove_duplicates.ts');
+jest.mock('../src/duplicates.ts');
 
 // Come back later and test this properly
 describe('dedup()', () => {
   beforeAll(() => {
     silenceOutput();
   });
-  it('returns a void promise', async () => {
+  it('calls certain functions with expected args and returns a void promise', async () => {
     const options: DedupOptions = {
-      dirsToPossiblyDeleteFrom: [],
+      dirsToPossiblyDeleteFrom: ['/tmp/tmp/foo'],
       exclude: [],
       includeDotfiles: false,
       interactiveDeletion: false,
-      pathsToTraverse: [],
-      reallyDelete: false,
+      pathsToTraverse: ['/tmp'],
+      reallyDelete: true,
     };
+    type GCF = jest.Mocked<typeof gcf.getCandidateFiles>;
+    (gcf.getCandidateFiles as GCF).mockReturnValue([
+      '/tmp/foo',
+      '/tmp/bar',
+      '/tmp/baz',
+    ]);
+    type HashFile = jest.Mocked<typeof hash_file.hashFile>;
+    (hash_file.hashFile as HashFile).mockImplementation(
+      (_file, onHashComplete) => {
+        onHashComplete();
+      }
+    );
+    // type RD = jest.Mocked<typeof rd.deleteOrListDuplicates>;
+    type GD = jest.Mocked<typeof duplicates.getDuplicates>;
 
+    const getDuplicatesRet = [['/tmp/foo', '/tmp/bar']];
+    (duplicates.getDuplicates as GD).mockReturnValue(getDuplicatesRet);
     const got = await dedup(options);
+
+    expect(gcf.getCandidateFiles).toHaveBeenCalledTimes(1);
+    expect(gcf.getCandidateFiles).toHaveBeenNthCalledWith(1, options);
+
+    expect(hash_file.hashFile).toHaveBeenCalledTimes(3);
+
+    expect(duplicates.getDuplicates).toHaveBeenCalledTimes(1);
+    expect(duplicates.getDuplicates).toHaveBeenCalledWith([]);
+
+    expect(rd.deleteOrListDuplicates).toHaveBeenCalledTimes(1);
+    expect(rd.deleteOrListDuplicates).toHaveBeenCalledWith(
+      getDuplicatesRet,
+      options.dirsToPossiblyDeleteFrom,
+      options.reallyDelete,
+      options.interactiveDeletion
+    );
+
     const expected = undefined;
     expect(got).toEqual(expected);
   });
