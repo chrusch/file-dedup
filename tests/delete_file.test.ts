46596e2803cdc9550f7b1398cb62b5cbc2a04717
Clayton Chrusch <chrusch@gmail.com>
2023-01-12 22:49:57 -0500

    update copyright notice.

--- tests/delete_file.test.ts
+++ tests/delete_file.test.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Clayton Chrusch
+// Copyright (c) 2022-2023, Clayton Chrusch
 // All rights reserved.
 //
 // This source code is licensed under the BSD-style license found in the
