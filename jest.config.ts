a4b25cf56820a066e57f55268afb7b0299b9302f
Clayton Chrusch <chrusch@gmail.com>
2023-02-07 17:32:41 -0500

    update jest.config.ts to look for tests in src directory

--- jest.config.ts
+++ jest.config.ts
@@ -153,11 +153,12 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
-  testMatch: ['**/tests/**/*.test.ts'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    // '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)',
+    // '<rootDir>/tests/**/*.test.ts',
+  ],
+  // testMatch: ['**/tests/**/*.test.ts'],
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
   //   "/node_modules/"
