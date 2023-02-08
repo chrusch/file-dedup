ba0375780fa23c231555d64a48079be0d8a8e044
Clayton Chrusch <chrusch@gmail.com>
2023-02-08 15:06:26 -0500

    remove unnecessary an inaccurate file name in file.

--- src/hash_file/hash_extractor.ts
+++ src/hash_file/hash_extractor.ts
@@ -4,8 +4,6 @@
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 
-// ./src/hash_extractor.ts
-//
 // Expects the stdout output of the following command:
 // shasum -a 256 <file>
 // This output might look like this:
